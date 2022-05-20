0d126976c989a7cea2300e0cc8038d1e3c1882c8
Patrick Louis <patrick.louis898@gmail.com>
2022-05-20 11:26:00 +0700

    NEW

---
+++ Travoila/New Group/LocalNotification.swift
@@ -0,0 +1,86 @@
+//
+//  LocalNotification.swift
+//  Travoila
+//
+//  Created by Patrick Louis on 20/05/22.
+//
+import SwiftUI
+import UserNotifications
+
+class NotificationManager {
+    
+    static let instance = NotificationManager()
+    
+    func requestAuthorization() {
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { (success, error) in
+            if let error = error {
+                print("ERROR: \(error)")
+            } else {
+                print("SUCCESS")
+            }
+        }
+    }
+    
+    func scheduleNotification() {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Keep On Track!"
+        content.subtitle = "Have you recorded today's expenses?"
+        content.sound = .default
+        content.badge = 1
+        
+        // Time
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        
+        // Calender
+        var dateComponents = DateComponents ()
+        dateComponents.hour = 21
+        dateComponents.minute = 00
+//        dateComponents.hour = 10
+//        dateComponents.minute = 48
+//        dateComponents.day = 1
+        
+
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
+        
+    }
+    func cancelNotification() {
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+    }
+}
+
+
+
+struct LocalNotification: View {
+    var body: some View {
+        VStack(spacing: 40) {
+            Button("Request permission") {
+                NotificationManager.instance.requestAuthorization()
+            }
+            Button("Schedule Notification") {
+                NotificationManager.instance.scheduleNotification()
+            }
+            Button("Cancel Notification") {
+                NotificationManager.instance.cancelNotification()
+            }
+        }
+        .onAppear {
+            UIApplication.shared.applicationIconBadgeNumber = 0
+        }
+    }
+}
+
+struct LocalNotification_Previews: PreviewProvider {
+    static var previews: some View {
+        LocalNotification()
+    }
+}
---
+++ Travoila/New Group/NewTripView.swift
@@ -0,0 +1,92 @@
+//
+//  Newtrip.swift
+//  Travoila
+//
+//  Created by Patrick Louis on 15/05/22.
+//
+import SwiftUI
+
+struct NewTripView: View {
+    
+    @State var tripTitle: String = ""
+    @State var tripDestination: String = ""
+    
+    @State var startDate: Date = Date()
+    @State var endDate: Date = Date()
+    
+    @State var totalBudgetEstimation: String = ""
+    @State var listRowColor: Color = Color.gray.opacity(0.08)
+    
+    @Binding var trips: [Trip]
+    @Binding var isNoTrip: Bool
+    
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    
+    var body: some View {
+        VStack {
+            Form {
+                Section ( header: Text("Trip Title")) {
+                    TextField("Trip Title", text: $tripTitle)
+                        .listRowBackground(listRowColor)
+                }
+                
+                Section ( header: Text("Trip Destination")) {
+                    TextField("Trip Destination", text: $tripDestination)
+                        .listRowBackground(listRowColor)
+                }
+                
+                Section(header: Text("Date")){
+                    DatePicker("Start Date", selection: $startDate, displayedComponents: .date)
+                        .listRowBackground(listRowColor)
+                    DatePicker("End Date", selection: $endDate, displayedComponents: .date)
+                        .listRowBackground(listRowColor)
+                }
+                
+                Section(header: Text("Total Budget Estimation")){
+                    TextField("IDR 0",text: $totalBudgetEstimation)
+                        .listRowBackground(listRowColor)
+                        .keyboardType(.default)
+                }
+            }
+            .onAppear {
+                UITableView.appearance().backgroundColor = .clear
+            }
+            .onDisappear {
+                UITableView.appearance().backgroundColor = .systemGroupedBackground
+            }
+            
+            Button(action: {
+                createNewTrip()
+            }){
+                Text("Save")
+                    .bold()
+                    .frame(width: 340, height: 50)
+                    .background(Color("CustomColor"))
+                    .foregroundColor(.white)
+                    .cornerRadius(10.0)
+            }
+            .frame(minWidth:0,  maxWidth: .infinity)
+        }
+        .navigationTitle("New Trip")
+    }
+    
+    func createNewTrip(){
+        trips.append(
+            Trip(title: tripTitle, destination: tripDestination, startDate: startDate, endDate: endDate, totalBudgetEstimation: Int(totalBudgetEstimation) ?? 0, categories: [] )
+        )
+        
+        isNoTrip = false
+        
+        mode.wrappedValue.dismiss()
+    }
+}
+
+struct NewTripView_Previews: PreviewProvider {
+    
+    @State private static var dummyData: [Trip] = []
+    @State private static var isNoTrip: Bool = true
+    
+    static var previews: some View {
+        NewTripView(trips: $dummyData, isNoTrip: $isNoTrip)
+    }
+}
