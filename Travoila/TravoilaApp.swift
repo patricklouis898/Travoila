0cc149d2726aebd872287cb7666e091537d935de
Patrick Louis <patrick.louis898@gmail.com>
2022-05-20 10:39:10 +0700

    Initial Commit

---
+++ Travoila/TravoilaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravoilaApp.swift
+//  Travoila
+//
+//  Created by Patrick Louis on 20/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TravoilaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
